a9c22a0f7c909c73e14ee1486372a54698c3e52a
XhavitO <xhavit.osaj@outlook.fr>
2021-11-04 17:37:56 +0100

    some db modification

--- src/main/java/fr/uge/friday/repository/TaskRepository.java
+++ src/main/java/fr/uge/friday/repository/TaskRepository.java
@@ -1,6 +1,6 @@
-package fr.uge.friday.dbManager.repository;
+package fr.uge.friday.repository;
 
-import fr.uge.friday.dbManager.entity.Task;
+import fr.uge.friday.entity.Task;
 import org.springframework.data.repository.CrudRepository;
 import java.util.Date;
 import java.util.List;
