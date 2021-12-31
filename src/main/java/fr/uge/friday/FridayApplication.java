063c40da57572b9a8c9c446eccbdea43dda2e6bf
Koptyxx <samyghamri77@gmail.com>
2021-12-31 23:45:25 +0100

    final real real push

--- src/main/java/fr/uge/friday/FridayApplication.java
+++ src/main/java/fr/uge/friday/FridayApplication.java
@@ -1,11 +1,7 @@
 package fr.uge.friday;
 
-import fr.uge.friday.repository.EventRepository;
-import fr.uge.friday.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -13,12 +9,5 @@ public class FridayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FridayApplication.class, args);
-    }
-    @Bean
-    public CommandLineRunner run(UserRepository userRepository, EventRepository eventRepository) {
-        return (args -> {
-            eventRepository.deleteAll();
-            userRepository.deleteAll();
-        });
     }
 }
